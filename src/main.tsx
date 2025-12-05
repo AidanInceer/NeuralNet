9fefd29a634d3b4eb076e30a802ad3a6101957da
Aidan Inceer <aidaninceer0@gmail.com>
2025-12-05 21:32:26 +0000

    feat: bootstrap React application with React DOM and add GitHub Pages configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
+console.log('Starting Neural Network Visualizer...');
 const rootElement = document.getElementById('root');
 if (rootElement) {
     createRoot(rootElement).render(
